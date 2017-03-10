55e4f994222fbb63c699683f630f92342a937f1b
arunkishore <ramakrishnanak@utwks62378.home>
2017-03-10 20:56:47 +0100

    aesthetic

--- filter+basicFFT/FFT.py
+++ filter+basicFFT/FFT.py
@@ -8,15 +8,10 @@ N = 128
 # sample spacing
 T = 1.0 / 128
 x = np.linspace(0.0, N*T, N)
-y = y#X_train.iloc[7350]  
+y = X_train.iloc[7350]  # compare the raw data with the low-pass filtered data
 yf = scipy.fftpack.fft(y)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
 fig, ax = plt.subplots()
 ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
 plt.show()
-
-
-# which data to use?
-# An example to get started with the low pass filter
-# An example for FFT analysis?
--- filter+basicFFT/lowpassFilter.py
+++ filter+basicFFT/lowpassFilter.py
@@ -18,7 +18,7 @@ def butter_lowpass_filter(data, cutoff, fs, order=5):
 # Filter requirements.
 order = 4
 fs = 50.0       # sample rate, Hz
-cutoff = 0.5# desired cutoff frequency of the filter, Hz
+cutoff = 5# desired cutoff frequency of the filter, Hz
 
 # Get the filter coefficients so we can check its frequency response.
 b, a = butter_lowpass(cutoff, fs, order)
@@ -40,8 +40,8 @@ plt.grid()
 T = 2.56         # seconds
 n = int(T * fs) # total number of samples
 t = np.linspace(0, T, n, endpoint=False)
-# "Noisy" data.  We want to recover the 1.2 Hz signal from this.
-#data = np.sin(1.2*2*np.pi*t) + 1.5*np.cos(9*2*np.pi*t) + 0.5*np.sin(12.0*2*np.pi*t)
+# "Noisy" data.  
+
 X_train = pd.read_csv('/Users/ramakrishnanak/Downloads/UCI HAR Dataset/train/Inertial Signals/total_acc_z_train.txt', delim_whitespace=True, header=None)
 data = X_train.iloc[7350]
 
